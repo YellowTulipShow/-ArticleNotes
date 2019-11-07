3b363d438dbf5031c9e9f935d8d31646392aedf1
YellowTulipShow <1426689530@qq.com>
2019-11-07 17:29:58 +0800

    add server/ .bat

---
+++ window/server/service_start.bat
@@ -0,0 +1,5 @@
+net stop ErTuiShengShi.QuartzService
+sc delete ErTuiShengShi.QuartzService
+.\bin\Debug\ErTuiShengShi.QuartzService.exe install
+net start ErTuiShengShi.QuartzService
+TIMEOUT /T -1
---
+++ window/server/service_stop.bat
@@ -0,0 +1,3 @@
+net stop ErTuiShengShi.QuartzService
+sc delete ErTuiShengShi.QuartzService
+TIMEOUT /T -1
