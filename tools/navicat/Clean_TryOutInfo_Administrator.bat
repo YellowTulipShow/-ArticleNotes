150054ce5d808d7f97b42b4ba3ea72802a3a6eb1
YellowTulipShow <main@yellowtulipshow.site>
2021-08-28 17:25:19 +0800

    脚本优化

--- tools/navicat/Clean_TryOutInfo_Administrator.bat
+++ tools/navicat/Clean_TryOutInfo_Administrator.bat
@@ -1,9 +1,11 @@
 @echo off
 
+chcp 65001
+
 echo\
 echo\
-echo 适用于 Navicat 12.0.29
-echo 正在清除试用信息。。。
+echo "适用于 Navicat 12.0.29"
+echo "正在清除试用信息。。。"
 
 for /f %%i in ('reg query HKCU\Software\Classes\CLSID') do call:checkDelCLSID %%i
 
@@ -22,8 +24,8 @@ rmdir /S /Q C:\Users\Administrator\AppData\Local\Temp\NavicatCloud
 
 echo\
 echo\
-echo 清除试用信息成功！
-echo 请重新运行Navicat。
+echo "清除试用信息成功！"
+echo "请重新运行Navicat。"
 echo\
 pause
 exit
