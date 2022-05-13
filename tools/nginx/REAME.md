94738dc84ace82baf2bd8e1c9a33a176b478d495
YellowTulipShow <main@yellowtulipshow.site>
2022-05-13 11:32:09 +0800

    nginx

---
+++ tools/nginx/REAME.md
@@ -0,0 +1,19 @@
+# Nginx Web反向代理服务器
+
+## 使用
+
+### 关闭 Windows 版
+
+查看nginx的进程占用情况
+```cmd
+tasklist | find /i "nginx.exe" || exit
+```
+
+关闭nginx的所有进程
+```cmd
+taskkill /im nginx.exe /f
+```
+
+## 链接
+
+* [官网下载地址](https://nginx.org/en/download.html)
---
+++ tools/nginx/_open.bat
@@ -0,0 +1,22 @@
+@echo off
+::切换执行环境编码为UTF-8
+@chcp 65001
+
+::获取=> 环境路径
+set execute_environment_path=%cd%
+::切换=> 当前脚本文件路径
+cd /d %~dp0
+::获取=> 当前脚本文件路径
+set script_file_path=%cd%
+
+echo 开启 Nginx
+
+.\close.bat
+.\nginx.exe
+
+echo 执行完毕!
+
+::切换=> 环境路径
+cd /d %execute_environment_path%
+::判断=> 当前脚本文件路径==环境路径 执行用户等待命令
+if %execute_environment_path%==%script_file_path% TIMEOUT /T -1
